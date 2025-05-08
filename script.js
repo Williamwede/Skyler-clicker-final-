2366e954f8cdd253517f1db109549f9a20b021c8
Williamwede <wgwedekemper@icloud.com>
2025-05-08 06:37:38 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+let cookies = 0;
+let multiplier = 1;
+
+function setMultiplier(value) {
+    multiplier = value;
+}
+
+function clickCookie() {
+    cookies += multiplier;
+    document.getElementById('cookie-count').innerText = cookies;
+}
